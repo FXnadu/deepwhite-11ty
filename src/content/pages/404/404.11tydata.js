3625dd3bfaa866c41379202d1568c8638698b5dc
FXnadu <122216161+FXnadu@users.noreply.github.com>
2025-11-26 23:38:11 +0800

    test

---
+++ src/content/pages/404/404.11tydata.js
@@ -0,0 +1,10 @@
+module.exports = {
+  layout: "base.njk",
+  title: "页面未找到",
+  permalink: "/404.html",
+  eleventyExcludeFromCollections: true,
+  islands: ["site"],
+  showFloatingActions: true,
+};
+
+
