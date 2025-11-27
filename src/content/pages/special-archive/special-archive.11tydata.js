7276a187a6e707e194be64168a82021fa451c378
FXnadu <122216161+FXnadu@users.noreply.github.com>
2025-11-28 00:05:01 +0800

    新增特殊归档页面模板

---
+++ src/content/pages/special-archive/special-archive.11tydata.js
@@ -0,0 +1,18 @@
+const paginateUrl = (pageNumber = 0) =>
+  pageNumber === 0
+    ? "/special-archive/"
+    : `/special-archive/page/${pageNumber + 1}/`;
+
+module.exports = {
+  layout: "base.njk",
+  title: "特别归档",
+  showFloatingActions: true,
+  isSpecialArchivePage: true,
+  eleventyComputed: {
+    permalink: (data) => paginateUrl(data.pagination?.pageNumber || 0),
+    hasSpecialEntries: (data) =>
+      Array.isArray(data.collections?.specialArchivePages) &&
+      data.collections.specialArchivePages.length > 0,
+  },
+};
+
