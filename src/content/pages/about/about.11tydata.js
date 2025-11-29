f88bcdfb0013f144b9ee8b5fcf660a7a320f522b
FXnadu <122216161+FXnadu@users.noreply.github.com>
2025-11-29 15:11:19 +0800

    fix: 为 about.css 添加版本号，修复远程部署后样式不生效的问题

--- src/content/pages/about/about.11tydata.js
+++ src/content/pages/about/about.11tydata.js
@@ -1,9 +1,14 @@
+// 读取 assets.js 中的版本号
+const assets = require("../../../_data/assets.js")();
+const versionMatch = assets.mainCssHref.match(/\?v=([^&]+)/);
+const versionSuffix = versionMatch ? versionMatch[1] : "";
+
 module.exports = {
   layout: "base.njk",
   title: "关于我",
   permalink: "/about/",
   islands: ["site"],
-  extraCss: ["/css/about.css"],
+  extraCss: [`/css/about.css${versionSuffix ? `?v=${versionSuffix}` : ""}`],
 };
 
 
